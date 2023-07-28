be7e1f20f2a908b71024e1b184dd688e932978a4
Douglas Holanda <douglas@heavyconnect.com>
2023-07-28 17:55:35 -0300

    Inserindo atualizações dos exercicios

--- PythonExercicios/Exe21.py
+++ PythonExercicios/Exe21.py
@@ -1,3 +1,10 @@
-# Exercicio 21 de Python - Deixando tudo pronto
+# Exercicio 21 de Python - Tocando um MP3
 
-# Crie um programa que escreva "Olá Mundo!" na tela.
+# Faça um programa em Python que abra e reproduza o áudio de um aquivo MP3.
+
+import pygame
+pygame.init()
+pygame.mixer.music.load('exe21.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
