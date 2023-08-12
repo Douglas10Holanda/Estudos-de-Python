0259354b6f2d93124f5a11e106ee3f37cb8b0396
Douglas Holanda <douglas@heavyconnect.com>
2023-08-11 21:37:01 -0300

    Novos Exercicios

--- PythonExercicios/Exe29.py
+++ PythonExercicios/Exe29.py
@@ -1,3 +1,16 @@
-# Exercicio 29 de Python - Tocando um MP3
+# Exercicio 29 de Python - Radar eletrônico
 
-# Faça um programa q
+"""
+Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre uma mensagem
+dizendo que ele foi multado. A multa vai custar R$ 7,00 por cada Km acima do limite.
+"""
+
+vel = float(input('Qual foi sua velocidade? '))
+multa = 0
+
+if vel > 80:
+    multa = 7*(vel-80)
+    print('Você foi multado!')
+    print('A multa vai lhe custar R$ {:.2f}'.format(multa))
+else:
+    print('Sem multas!')
--- PythonExercicios/Exe30.py
+++ PythonExercicios/Exe30.py
@@ -1,3 +1,11 @@
-# Exercicio 30 de Python - Tocando um MP3
+# Exercicio 30 de Python - Par ou Ímpar?
 
-# Faça um programa q
+# Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+n = int(input('Digite um número: '))
+r = n%2
+
+if r == 0:
+    print('O número {} é par!'.format(n))
+else:
+    print('O número {} é ímpar!'.format(n))
